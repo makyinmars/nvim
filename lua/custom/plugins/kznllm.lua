b322a8c64c4eee6c941fe1890ba718565bf2bc5e
Franklin <franklinjaradev@gmail.com>
2024-08-31 23:07:14 -0400

    adding debug mode for kznllm

--- lua/custom/plugins/kznllm.lua
+++ lua/custom/plugins/kznllm.lua
@@ -14,6 +14,13 @@ return {
       }, spec_anthropic.make_job)
     end
 
+    local function llm_anthropic_fill_debug()
+      kznllm.invoke_llm({
+        { role = 'system', prompt_template = spec_anthropic.PROMPT_TEMPLATES.FILL_MODE_SYSTEM_PROMPT },
+        { role = 'user', prompt_template = spec_anthropic.PROMPT_TEMPLATES.FILL_MODE_USER_PROMPT },
+      }, spec_anthropic.make_job, { debug = true })
+    end
+
     local function llm_groq_fill()
       kznllm.invoke_llm({
         { role = 'system', prompt_template = spec_groq.PROMPT_TEMPLATES.GROQ.FILL_MODE_SYSTEM_PROMPT },
@@ -21,7 +28,17 @@ return {
       }, spec_groq.make_job)
     end
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>kab', llm_anthropic_fill, { desc = 'Send current selection to LLM ANTHROPIC llm_fill' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>kgb', llm_groq_fill, { desc = 'Send current selection to LLM GROQ llm_buffer' })
+    local function llm_groq_fill_debug()
+      kznllm.invoke_llm({
+        { role = 'system', prompt_template = spec_groq.PROMPT_TEMPLATES.GROQ.FILL_MODE_SYSTEM_PROMPT },
+        { role = 'user', prompt_template = spec_groq.PROMPT_TEMPLATES.GROQ.FILL_MODE_USER_PROMPT },
+      }, spec_groq.make_job, { debug = true })
+    end
+
+    vim.keymap.set({ 'n', 'v' }, '<leader>kab', llm_anthropic_fill, { desc = 'Send current selection to LLM ANTHROPIC buffer' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>kad', llm_anthropic_fill_debug, { desc = 'Send current selection to LLM ANTHROPIC DEBUG' })
+
+    vim.keymap.set({ 'n', 'v' }, '<leader>kgb', llm_groq_fill, { desc = 'Send current selection to LLM GROQ buffer' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>kgd', llm_groq_fill_debug, { desc = 'Send current selection to LLM GROQ DEBUG' })
   end,
 }
