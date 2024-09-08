6e470f2f7dcb7c449a416e279e1d51f042b03ab0
Franklin <franklinjaradev@gmail.com>
2024-09-08 01:20:28 -0400

    supermaven stop by default, use <leader>ct to toggle it

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,11 +1,24 @@
 return {
   'supermaven-inc/supermaven-nvim',
+  cmd = {
+    'SupermavenStart',
+    'SupermavenStop',
+    'SupermavenRestart',
+    'SupermavenToggle',
+    'SupermavenStatus',
+    'SupermavenUseFree',
+    'SupermavenUsePro',
+    'SupermavenLogout',
+    'SupermavenShowLog',
+    'SupermavenClearLog',
+  },
   config = function()
     require('supermaven-nvim').setup {
       keymaps = {
         accept_suggestion = '<C-y>',
       },
       ignore_filetypes = { env = true, zshrc = true },
+      disable_inline_completion = false, -- disables inline completion for use with cmp
     }
   end,
   keys = {
