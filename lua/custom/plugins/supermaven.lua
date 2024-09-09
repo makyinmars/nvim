ff9631520f197a229a9f6bd39063a059a8b9d2c7
Franklin <franklinjaradev@gmail.com>
2024-09-08 20:08:12 -0400

    updating supermaven

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,27 +1,15 @@
 return {
   'supermaven-inc/supermaven-nvim',
-  cmd = {
-    'SupermavenStart',
-    'SupermavenStop',
-    'SupermavenRestart',
-    'SupermavenToggle',
-    'SupermavenStatus',
-    'SupermavenUseFree',
-    'SupermavenUsePro',
-    'SupermavenLogout',
-    'SupermavenShowLog',
-    'SupermavenClearLog',
-  },
   config = function()
-    require('supermaven-nvim').setup {
-      keymaps = {
-        accept_suggestion = '<C-y>',
-      },
-      ignore_filetypes = { env = true, zshrc = true, zig = true },
-      disable_inline_completion = false, -- disables inline completion for use with cmp
-    }
+    local function enable()
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<C-y>',
+        },
+        ignore_filetypes = { env = true },
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+      }
+    end
+    vim.keymap.set('n', '<leader>ct', enable, { desc = '[s]uper[m]aven' })
   end,
-  keys = {
-    { '<leader>ct', '<cmd>SupermavenToggle<cr>', desc = 'Supermaven Toggle' },
-  },
 }
