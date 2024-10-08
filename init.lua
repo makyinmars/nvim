fd6c6a3df413b99ee1a07206d03ddc3bb1561b73
Franklin <franklinjaradev@gmail.com>
2024-10-08 19:55:44 -0400

    adding supermaven

--- init.lua
+++ init.lua
@@ -364,6 +364,7 @@ require('lazy').setup({
   require 'custom.plugins.tmux-navigation',
   require 'custom.plugins.crackboard',
   require 'custom.plugins.obsidian',
+  require 'custom.plugins.supermaven',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
