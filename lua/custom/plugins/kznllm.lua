e7b6348b7b9aaba2a5bd4a0aeabfa8cece623e83
Franklin <franklinjaradev@gmail.com>
2024-12-31 10:44:28 -0500

    update clean up

--- lua/custom/plugins/kznllm.lua
+++ lua/custom/plugins/kznllm.lua
@@ -1,10 +1,39 @@
 return {
   'chottolabs/kznllm.nvim',
+  dev = true,
+  dir = os.getenv 'HOME' .. '/Development/OpenSource/kznllm.nvim',
   dependencies = {
     { 'j-hui/fidget.nvim' },
   },
-  config = function(self)
+  config = function(_)
     local presets = require 'kznllm.presets.basic'
+
+    local function yap_generator()
+      math.randomseed(os.time())
+      local yap_cycle = {
+        'yapped for %ds',
+        '...',
+      }
+
+      local idx = math.random(1, #yap_cycle)
+      return function()
+        idx = idx + 1
+        if idx > #yap_cycle then
+          idx = 1
+        end
+        return yap_cycle[idx]
+      end
+    end
+
+    local yap = yap_generator()
+
+    local function progress_fn(state)
+      local now = os.time()
+      if (now ~= state.last_updated) and ((now - state.start) % 3) == 0 then
+        state.last_updated = now
+        return yap()
+      end
+    end
 
     vim.keymap.set({ 'n', 'v' }, '<leader>kdm', function()
       presets.switch_presets(presets.options)
@@ -17,8 +46,18 @@ return {
       end
     end
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>kdd', invoke_with_opts { debug = true }, { desc = 'Send current selection to LLM debug' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>kdb', invoke_with_opts { debug = false }, { desc = 'Send current selection to LLM llm_fill' })
+    vim.keymap.set(
+      { 'n', 'v' },
+      '<leader>kdb',
+      invoke_with_opts { debug = false, progress_message_fn = progress_fn },
+      { desc = 'Send current selection to LLM llm_fill' }
+    )
+    vim.keymap.set(
+      { 'n', 'v' },
+      '<leader>kdd',
+      invoke_with_opts { debug = true, progress_message_fn = progress_fn },
+      { desc = 'Send current selection to LLM debug' }
+    )
 
     vim.api.nvim_set_keymap('n', '<Esc>', '', {
       noremap = true,
